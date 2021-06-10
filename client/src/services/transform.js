f1f500f2b0360d80ecbdafaddcfd8aa4bc45907a
Said <sacred42rus@mail.ru>
2021-06-10 15:01:24 +0700

    interface is done

--- client/src/services/transform.js
+++ client/src/services/transform.js
@@ -1,21 +1,12 @@
 const getSortedCurrency = (values, nameCurrency) => {
-
-const transArr = transformDates(values, nameCurrency );
-
-return transArr;
-}
-
-const transformDates = (values , nameCurrency) => {
-   return [values[0][nameCurrency], values[1][nameCurrency], values[2][nameCurrency]];
+    return values.map((elem)=>elem[nameCurrency]);
 }
 
 const transformBaseCurrency = (values) => {
- console.log(values , 'должен быть массив')
  return getSmaller(values);
 }
 
 const getTransformCurrencyPair = (currency1, currency2) => {
- console.log(currency1 , currency2 , 'два массива');
  const arr = [];
  for(let i = 0; i < currency1.length; i++){
      for(let j = 0; j < currency2.length; j++){
@@ -49,7 +40,6 @@ const setObj = (arr) => {
     const obj = {
     ...arr
     }
-    console.log(obj)
     let index = 0;
     const newObj = {}
     
